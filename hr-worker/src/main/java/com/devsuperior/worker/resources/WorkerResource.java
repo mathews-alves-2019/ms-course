de83fa455c96baf0f3ee57bf37537721ffe36a3e
mathews <mathews.alves.2019@outlook.com>
2021-04-25 21:42:12 -0300

    Actuator for runtime config refresh

--- hr-worker/src/main/java/com/devsuperior/worker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/worker/resources/WorkerResource.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,11 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.devsuperior.worker.entities.Worker;
 import com.devsuperior.worker.repositories.WorkerRepository;
 
+@RefreshScope
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 	
 	private static Logger Logger = LoggerFactory.getLogger(WorkerResource.class);
+	
+	@Value("${test.config}")
+	private String testConfig;
 	
 	@Autowired
 	private Environment env;
@@ -31,6 +37,13 @@ public class WorkerResource {
 	public ResponseEntity<List<Worker>> findAll() {
 		List<Worker> list = repository.findAll();
 		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value="/configs")
+	public ResponseEntity<Void> getConfigs() {
+		Logger.info("Config = " + testConfig);
+		
+		return ResponseEntity.noContent().build();
 	}
 	
 	@GetMapping(value = "/{id}")
